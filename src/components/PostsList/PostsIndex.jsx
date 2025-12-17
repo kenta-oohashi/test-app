4aa10b09bc6e3c48051d4754596691e1e1a21970
kenta oohashi <kerutomusic@gmail.com>
2025-12-18 00:07:44 +0900

    6章課題3_提出１回目

--- src/components/PostsList/PostsIndex.jsx
+++ src/components/PostsList/PostsIndex.jsx
@@ -1,24 +1,48 @@
-import { posts } from "../../data/posts";
-import style from './Posts.module.css';
+import style from "./Posts.module.css";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function PostsIndex() {
-    return (
-      <div className={style["post-list-container"]}>
-        {posts.map((post) => (
-          <Link key={post.id} to={`/posts/${post.id}`} className={style["post-card"]}>
-            <div className={style["post-card-info"]}>
-              <div className={style["create-data"]}>{new Date(post.createdAt).toLocaleDateString()}</div>
-              <div className={style["post-categories"]}>
-                {post.categories.map((category) => (
-                    <span key={category} className={style["post-tag"]}>{category}</span>
-                ))}
-              </div>
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetcher = async () => {
+      const res = await fetch(
+        "https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts"
+      );
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetcher();
+  }, []);
+
+  return (
+    <div className={style["post-list-container"]}>
+      {posts.map((post) => (
+        <Link
+          key={post.id}
+          to={`/posts/${post.id}`}
+          className={style["post-card"]}
+        >
+          <div className={style["post-card-info"]}>
+            <div className={style["create-data"]}>
+              {new Date(post.createdAt).toLocaleDateString()}
             </div>
-            <h2 className={style["post-title"]}>{post.title}</h2>
-            <div className={style["post-content"]} dangerouslySetInnerHTML={{ __html: post.content }}></div>
-          </Link>
-        ))}
-      </div>
-    );
+            <div className={style["post-categories"]}>
+              {post.categories.map((category) => (
+                <span key={category} className={style["post-tag"]}>
+                  {category}
+                </span>
+              ))}
+            </div>
+          </div>
+          <h2 className={style["post-title"]}>{post.title}</h2>
+          <div
+            className={style["post-content"]}
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          ></div>
+        </Link>
+      ))}
+    </div>
+  );
 }
